ad7082fc6dfd55c687408136039795e8b6c7ed27
dariayuzh <yuzhakovadasha@gmail.com>
2022-05-21 20:42:01 +0300

    Test 2

--- 1_Introduction/src/MainClassTest.java
+++ 1_Introduction/src/MainClassTest.java
@@ -1,10 +1,17 @@
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class MainClassTest {
+    MainClass mainClass = new MainClass();
+
     @Test
     public void testGetLocalNumber() {
-        MainClass mainClass = new MainClass();
         assertEquals("getLocalNumber does not return 14", 14, mainClass.getLocalNumber());
+    }
+
+    @Test
+    public void testGetClassNumber() {
+        assertTrue("getClassNumber returns a number less than 45", mainClass.getClassNumber() > 45);
     }
 }
