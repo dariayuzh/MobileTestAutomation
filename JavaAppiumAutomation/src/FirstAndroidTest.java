886278fcb9259ca37edcc93c8bf534051758560b
dariayuzh <yuzhakovadasha@gmail.com>
2022-06-06 23:52:14 +0300

    Ex2

---
+++ JavaAppiumAutomation/src/FirstAndroidTest.java
@@ -0,0 +1,47 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class FirstAndroidTest {
+    private AppiumDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "AndroidTestDevice");
+        capabilities.setCapability("platformVersion", "8.0");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", ".main.MainActivity");
+        capabilities.setCapability("app", "D:/MyProjects/JavaAppiumAutomation/apks/org.wikipedia.apk");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void checkInputFieldText() {
+        assertElementHasText(By.xpath("//*[@resource-id='org.wikipedia:id/search_container']//android.widget.TextView"),
+                "Search Wikipedia",
+                "Cannot find text Search Wikipedia in input line");
+    }
+
+    private void assertElementHasText(By by, String expectedText, String errorMessage) {
+        WebElement element = driver.findElement(by);
+        String elementText = element.getText();
+        Assert.assertEquals(errorMessage, expectedText, elementText);
+    }
+}
